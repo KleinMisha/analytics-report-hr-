8becfbc3928324b8e22e89674e1dd4f22a05c4f3
KleinMisha <klein.misha@gmail.com>
2025-12-19 22:20:31 +0100

    First commit.

---
+++ main.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+DATA_PATH = Path(
+    "/Users/misha/Documents/work/HSR_Rotterdam_minor_data_science/hours_worked.xlsx"
+)
+
+
+def main():
+    print("Hello from analytics-report-hr!")
+
+
+if __name__ == "__main__":
+    main()
