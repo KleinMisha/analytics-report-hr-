dc6e2e456280fd5702dcc8b3cbe3cac44c99245e
KleinMisha <klein.misha@gmail.com>
2025-12-21 16:48:48 +0100

    Render a PDF from the .qmd file using the quarto CLI.

--- main.py
+++ main.py
@@ -1,12 +1,56 @@
-from pathlib import Path
+"""
+Use the quarto CLI to render a PDF report
+"""
 
-DATA_PATH = Path(
+from pathlib import Path
+import subprocess
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+# load environment variables
+load_dotenv(".env")
+
+
+# settings:
+INPUT_FILE = "working_hours_report.qmd"
+REPORTS_DIR = Path("./reports/")
+BASE_FILE_NAME = "working_hours_report"
+
+# parameters inside the markdown
+HOURLY_RATE: int = int(os.getenv("HOURLY_RATE", 0.0))
+DATA_PATH = (
     "/Users/misha/Documents/work/HSR_Rotterdam_minor_data_science/hours_worked.xlsx"
 )
 
 
 def main():
-    print("Hello from analytics-report-hr!")
+    today = datetime.today()
+    filename = f"{today:%Y%m%d}_{BASE_FILE_NAME}.pdf"
+    output = ["--output-dir", f"{REPORTS_DIR}", "--output", f"{filename}"]
+    parameters = [
+        "-P",
+        f"hourly_rate:{HOURLY_RATE}",
+        "-P",
+        f"data_path:{DATA_PATH}",
+    ]
+    quarto_render = ["quarto", "render", INPUT_FILE]
+
+    command = quarto_render + parameters + output
+    print(f"Running {' '.join(command)} ")
+    try:
+        subprocess.run(
+            command,
+            check=True,
+            capture_output=True,
+            text=True,
+        )
+        print(
+            f"\N{CHECK MARK} Successfully compiled {INPUT_FILE} \N{RIGHTWARDS ARROW} {REPORTS_DIR}/{filename}"
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"\N{CROSS MARK} Failed to compile {INPUT_FILE}")
+        print(f"    Error message: {e.stderr}")
 
 
 if __name__ == "__main__":
